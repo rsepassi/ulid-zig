aff8ae7afe97da7940f42e825eb4c1e9b9c2e556
Kevin Poalses <kevin.poalses@senapt.co.uk>
2024-10-17 12:33:13 +0100

    fix: Updated `build.zig` to work with Zig 0.13 build system

--- build.zig
+++ build.zig
@@ -5,14 +5,14 @@ pub fn build(b: *std.Build) !void {
     const optimize = b.standardOptimizeOption(.{});
 
     // Module
-    const ulid_module = b.addModule("ulid", .{ .source_file = .{ .path = "ulid.zig" } });
+    const ulid_module = b.addModule("ulid", .{ .root_source_file = b.path("ulid.zig") });
 
     // Static library
-    const ulid_lib = b.addStaticLibrary(.{ .name = "ulid", .root_source_file = .{ .path = "ulid.zig" }, .target = target, .optimize = optimize });
+    const ulid_lib = b.addStaticLibrary(.{ .name = "ulid", .root_source_file = b.path("ulid.zig"), .target = target, .optimize = optimize });
     ulid_lib.linkLibC();
     const lib_step = b.step("lib", "Build the ulid static library");
     lib_step.dependOn(&ulid_lib.step);
-    const install_h = b.addInstallHeaderFile("ulid.h", "ulid.h");
+    const install_h = b.addInstallHeaderFile(b.path("ulid.h"), "ulid.h");
     lib_step.dependOn(&install_h.step);
     const install_lib = b.addInstallArtifact(ulid_lib, .{});
     lib_step.dependOn(&install_lib.step);
@@ -20,10 +20,11 @@ pub fn build(b: *std.Build) !void {
     // Benchmark
     const ulid_bench = b.addExecutable(.{
         .name = "ulid-bench",
-        .root_source_file = .{ .path = "ulid_benchmark.zig" },
+        .root_source_file = b.path("ulid_benchmark.zig"),
+        .target = target,
         .optimize = .ReleaseFast,
     });
-    ulid_bench.addModule("ulid", ulid_module);
+    ulid_bench.root_module.addImport("ulid", ulid_module);
     ulid_bench.linkLibC();
     {
         const bench_step = b.step("benchmark", "Run the ulid benchmarks");
@@ -33,15 +34,16 @@ pub fn build(b: *std.Build) !void {
     // Example
     const ulid_example = b.addExecutable(.{
         .name = "ulid-example",
-        .root_source_file = .{ .path = "ulid_example.zig" },
+        .target = target,
+        .root_source_file = b.path("ulid_example.zig"),
     });
-    ulid_example.addModule("ulid", ulid_module);
+    ulid_example.root_module.addImport("ulid", ulid_module);
     const example_step = b.step("example", "Run the ulid example");
     example_step.dependOn(&b.addRunArtifact(ulid_example).step);
 
     // CLI
-    const ulid_bin = b.addExecutable(.{ .name = "ulid" });
-    ulid_bin.addCSourceFile(.{ .file = .{ .path = "ulid.c" }, .flags = &[_][]const u8{} });
+    const ulid_bin = b.addExecutable(.{ .name = "ulid", .target = target });
+    ulid_bin.addCSourceFile(.{ .file = b.path("ulid.c"), .flags = &[_][]const u8{} });
     ulid_bin.linkLibrary(ulid_lib);
     const bin_step = b.step("cli", "Build the ulid cli");
     bin_step.dependOn(&b.addInstallArtifact(ulid_bin, .{}).step);
@@ -50,11 +52,11 @@ pub fn build(b: *std.Build) !void {
     const tests = b.step("test", "Run tests");
     const ulid_test = b.addTest(.{
         .name = "ulid-test",
-        .root_source_file = .{ .path = "ulid.zig" },
+        .root_source_file = b.path("ulid.zig"),
     });
     ulid_test.linkLibC();
-    const ulid_ctest = b.addExecutable(.{ .name = "ulidc-test" });
-    ulid_ctest.addCSourceFile(.{ .file = .{ .path = "ulid.c" }, .flags = &[_][]const u8{"-DTEST"} });
+    const ulid_ctest = b.addExecutable(.{ .name = "ulidc-test", .target = target });
+    ulid_ctest.addCSourceFile(.{ .file = b.path("ulid.c"), .flags = &[_][]const u8{"-DTEST"} });
     ulid_ctest.linkLibrary(ulid_lib);
     tests.dependOn(&b.addRunArtifact(ulid_ctest).step);
     tests.dependOn(&b.addRunArtifact(ulid_test).step);
